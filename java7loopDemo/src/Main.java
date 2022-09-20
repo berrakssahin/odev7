5c28c749b405335705c924ec59e3e2a8ebe0465a
berrakssahin <112779654+berrakssahin@users.noreply.github.com>
2022-09-20 23:05:15 +0300

    Add files via upload

---
+++ java7loopDemo/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //for
+
+        for(int i=2; i<10; i+=2) {
+            System.out.println(i);
+        }
+
+        System.out.println("For döngüsü bitti");
+
+        //while
+
+
+        int i=1;
+
+        while(i<10) {
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("While döngüsü bitti");
+
+        //Do-while
+        int j=1;
+        do{
+            System.out.println(j);
+            j+=2;
+        }while(j<10);
+
+        System.out.println("Do-while döngüsü bitti");
+
+    }
+}
